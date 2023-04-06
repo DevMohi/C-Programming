6e43c8ae84635af10472cf0e65f29e105138d0d4
DevMohi <morphex72@gmail.com>
2023-04-06 23:06:12 +0800

    Exam Module 4

--- Module 3/loop_1.c
+++ Module 3/loop_1.c
@@ -2,7 +2,7 @@
 
 int main(){
     int i;
-    for(i=1; i<=3; i=i+1){
+    for(i=1; i<=5; i=i+1){
         printf("Hello\n");
     }
 }
---
+++ Module 4/exam.c
@@ -0,0 +1,91 @@
+//Problem 1 - Print It
+
+// #include<stdio.h>
+
+// int main(){
+//     printf("Hello, world! I am learning C programming language. ^_^\n");
+//     printf("Programming is fun and challenging. /\\/\\/\\\n");
+//     printf("I want to give my 100%% dedication to learn!\tI will succeed one day.");
+// }
+
+
+
+//Problem 2 - Multiply
+
+// #include<stdio.h>
+
+// int main(){
+//     long long int a,b;
+//     scanf("%lld %lld",&a,&b);
+
+//     long long int multiply = a*b;
+//     printf("%lld",multiply);
+// }
+
+
+
+//Problem 3 - Divisible
+
+// #include<stdio.h>
+
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+
+//     if(n>=0){
+//         if(n%3==0){
+//             printf("YES");
+//         }
+//         else{
+//             printf("NO");
+//         }
+//     }
+// }
+
+
+// Problem 4- Divisible 2 
+
+// #include<stdio.h>
+
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+
+//     for(int i=1; i<=n; i++){
+//         if(i%3==0 && i%7==0){
+//             printf("%d\n",i);
+//         }
+//     }
+// }
+
+
+//Problem 5 Shopping
+
+//Alisa will only buy shoes if you also buy a pair. 
+//and you will buy a pair of shoes if you can buy punjabi.
+//Punjabi nile shoes nibo 
+//500 taka or more thakle shoes kinbo
+
+
+//you will punjabi if money>1000
+
+
+#include<stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+
+    //punjabi kinbar condition
+    if(n>1000){
+        printf("I will buy Punjabi\n");
+        int deduct = n-1000;
+        if(deduct >= 500){
+            printf("I will buy new shoes\n");
+            printf("Alisa will buy new shoes\n");
+        }
+    }
+    else{
+        printf("Bad luck!\n");
+    }
+}
