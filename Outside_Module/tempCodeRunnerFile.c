8c339d0f4e7e7f61bc7ecdb64d8e36fd0963fc73
DevMohi <morphex72@gmail.com>
2023-04-29 23:48:50 +0800

    Module 13 start

--- Outside_Module/tempCodeRunnerFile.c
+++ Outside_Module/tempCodeRunnerFile.c
@@ -4,7 +4,7 @@
    long long int toy=0;
    scanf("%lld %lld %lld",&e,&m,&b);
    long long int sum = e+b;
-   for (long long int i = 0; i <= sum; i++)
+   for (int i = 0; i <= sum; i++)
     {
         if (e>=1 && m>=1 && b>=1)
         {
