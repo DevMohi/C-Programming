c29fa7e849f3a5ecd0ea96ba042599b123434290
DevMohi <morphex72@gmail.com>
2023-05-04 02:27:41 +0800

    Module 15 done

--- practice/practice.c
+++ practice/practice.c
@@ -150,42 +150,97 @@
 //Pattern 5
 // Two values equal one x 
 
-#include<stdio.h>
+// #include<stdio.h>
 
-int main(){
-    int n;
-    scanf("%d",&n);
+// int main(){
+//     int n;
+//     scanf("%d",&n);
 
-    int ar[n];
+//     int ar[n];
 
 
-    for(int i=0; i<n; i++){
-        scanf("%d",&ar[i]);
-    } 
+//     for(int i=0; i<n; i++){
+//         scanf("%d",&ar[i]);
+//     } 
 
-    int x;
-    scanf("%d",&x);
+//     int x;
+//     scanf("%d",&x);
 
-    int flag=0;
-    for(int i=0; i<n-1; i++){
-        for(int j=i+1; j<n; j++){
-            // printf("%d %d\n",ar[i],ar[j]); 
+//     int flag=0;
+//     for(int i=0; i<n-1; i++){
+//         for(int j=i+1; j<n; j++){
+//             // printf("%d %d\n",ar[i],ar[j]); 
             
              
-            if(ar[i] +ar[j] == x){
-                flag =1;
-            }   
-        }
-    }
+//             if(ar[i] +ar[j] == x){
+//                 flag =1;
+//             }   
+//         }
+//     }
 
-    if(flag==0){
-        printf("No");
-    }
-    else{
-        printf("YES");
-    }
+//     if(flag==0){
+//         printf("No");
+//     }
+//     else{
+//         printf("YES");
+//     }
 
     
 
-    return 0;
-}
+//     return 0;
+// }
+
+
+// 22 
+//  #include <stdio.h>
+
+//     void swap(int *p, int *q)
+//     {
+//         int temp = *p; *p = *q; *q = temp;
+//     }
+//     void main()
+//     {
+//         int a = 6, b = 5;
+//         swap(&a, &b);
+//         printf("%d %d\n", a, b);
+//     }
+
+
+// #include <stdio.h>
+//     void m(int *p)
+//     {
+//         int i = 0;
+//         for(i = 0;i < 5; i++)
+//         printf("%d\t", p[i]);
+//     }
+//     void main()
+//     {
+//         int a[5] = {6, 5, 3};
+//         m(&a);
+//     }
+
+//array 
+// #include <stdio.h>
+//     void m(int p, int q)
+//     {
+//         int temp = p;
+//         p = q;
+//         q = temp;
+//     }
+//     void main()
+//     {
+//         int a = 6, b = 5;
+//         m(a, b);
+//         printf("%d %d\n", a, b);
+//     }
+
+
+#include <stdio.h>
+    int main()
+    {
+        int ary[4] = {1, 2, 3, 4};
+        int *p;
+        p = ary + 3;
+        *p = 5;
+        printf("%d\n", ary[3]);
+    }
---
+++ practice/tempCodeRunnerFile.c
@@ -0,0 +1,4 @@
+    // void swap(int *p, int *q)
+    // {
+    //     int temp = *p; *p = *q; *q = temp;
+    // }
